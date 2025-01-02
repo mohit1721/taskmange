966edfb774f41ae51e3bb7a6016450d56acb668f
mohit1721 <mohitmandal192@gmail.com>
2025-01-02 16:25:00 +0530

    updated 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,20 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+// export default eslintConfig;
+
+// ----
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -5,10 +22,12 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Create a new FlatCompat instance
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+// Use FlatCompat to extend configurations
+const eslintConfig = compat.extends("next/core-web-vitals");
 
 export default eslintConfig;
