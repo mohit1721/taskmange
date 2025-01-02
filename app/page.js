0d4e9cd1adc85ab6eab5905ca0624a54117af03e
mohit1721 <mohitmandal192@gmail.com>
2025-01-02 21:36:53 +0530

    updated 5

--- app/page.js
+++ app/page.js
@@ -2,18 +2,19 @@
 import React, { useState } from 'react';
 import SignUpForm from '../components/SignUp';
 import SignInForm from '../components/SignIn';
+import Image from 'next/image';
 
 const App = () => {
   const [isSignUp, setIsSignUp] = useState(true);
-  const [isUserSignedUp, setIsUserSignedUp] = useState(false); // to track if user signed up
+  // const [isUserSignedUp, setIsUserSignedUp] = useState(false); // to track if user signed up
 
-  const handleSignUp = (formData) => {
-    console.log('Sign Up Data:', formData);
-    // You can replace this with API call logic for user sign-up
-     // Simulate a successful sign-up:
-     setIsUserSignedUp(true); // Mark that the user has signed up
-     setIsSignUp(false); // Redirect to Sign-In form
-  };
+  // const handleSignUp = (formData) => {
+  //   console.log('Sign Up Data:', formData);
+  //   // You can replace this with API call logic for user sign-up
+  //    // Simulate a successful sign-up:
+  //    setIsUserSignedUp(true); // Mark that the user has signed up
+  //    setIsSignUp(false); // Redirect to Sign-In form
+  // };
 
   const handleSignIn = (formData) => {
     console.log('Sign In Data:', formData);
@@ -23,7 +24,7 @@ const App = () => {
   return (
 <div className='flex flex-col md:flex-row gap-20'>
 <div className='hidden md:flex mx-auto align-center'>
-<img width={300} alt='logo' src='https://file.aiquickdraw.com/imgcompressed/img/compressed_df9f1e11c22ba61bdba5b0993b38ae03.webp'/>
+<Image width={300} alt='logo' src='https://file.aiquickdraw.com/imgcompressed/img/compressed_df9f1e11c22ba61bdba5b0993b38ae03.webp'/>
 </div>
   <div className="auth-container">
       <h1 className='text-blue-300 font-bold text-center'>{isSignUp ? 'Sign Up Form' : 'Sign In Form'}</h1>
