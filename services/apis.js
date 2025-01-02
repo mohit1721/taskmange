4ca0ea3d51f09d2ee0de155a47e8e4a5c3334c75
mohit1721 <mohitmandal192@gmail.com>
2025-01-02 16:14:34 +0530

    bbb

--- services/apis.js
+++ services/apis.js
@@ -1,6 +1,7 @@
 // const BASE_URL = process.env.REACT_APP_BASE_URL;
 // const BASE_URL=""
-const BASE_URL="http://localhost:5000/api/v1"
+// const BASE_URL="http://localhost:5000/api/v1"
+const BASE_URL= "https://taskmanagenbe.onrender.com/api/v1"
 export const authENDPOINTS={
     SIGNUP_API: BASE_URL + "/auth/register",
     LOGIN_API: BASE_URL + "/auth/login",
